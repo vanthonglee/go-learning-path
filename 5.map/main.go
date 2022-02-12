46c0e68c70f78e5546221cb6f039823bfd4f7fa5
vanthonglee <thonglevan131@gmail.com>
2022-02-12 18:41:50 +0700

    [map] manipulating map

--- 5.map/main.go
+++ 5.map/main.go
@@ -6,12 +6,21 @@ func main() {
 
 	// var colors map[string]string
 
-	// colors := make(map[string]string)
+	colors := make(map[int]string)
 
-	colors := map[string]string{
-		"red":   "#FF0000",
-		"green": "#00FF00",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#FF0000",
+	// 	"green": "#00FF00",
+	// }
+
+	// colors["white"] = "#ffffff"
+	// Cannot use this syntax to access white like struts
+	// structName.white
+	// instead, colors["white"] becuase all keys inside of our map are typed
+
+	colors[10] = "ten"
+
+	delete(colors, 10)
 
 	fmt.Println(colors)
 }
