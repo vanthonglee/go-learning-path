f12303a7d96ceb80146a911651a134227a2b112d
vanthonglee <thonglevan131@gmail.com>
2022-02-12 18:37:01 +0700

    another way to create map

--- 5.map/main.go
+++ 5.map/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
+
+	// var colors map[string]string
+
+	// colors := make(map[string]string)
+
 	colors := map[string]string{
 		"red":   "#FF0000",
 		"green": "#00FF00",
