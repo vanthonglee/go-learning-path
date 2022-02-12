2496f47cb89050f906ea7f245540f601198733c5
vanthonglee <thonglevan131@gmail.com>
2022-02-12 20:05:37 +0700

    [map] iterate map

--- 5.map/main.go
+++ 5.map/main.go
@@ -6,21 +6,27 @@ func main() {
 
 	// var colors map[string]string
 
-	colors := make(map[int]string)
-
-	// colors := map[string]string{
-	// 	"red":   "#FF0000",
-	// 	"green": "#00FF00",
-	// }
+	// colors := make(map[int]string)
 
 	// colors["white"] = "#ffffff"
 	// Cannot use this syntax to access white like struts
 	// structName.white
 	// instead, colors["white"] becuase all keys inside of our map are typed
 
-	colors[10] = "ten"
+	// colors[10] = "ten"
 
-	delete(colors, 10)
+	// delete(colors, 10)
 
-	fmt.Println(colors)
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#00FF00",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
